00b63f1dc7f1e0091a5bc7d8f423290e88384646
Senthil Kumar Ravichandran <innovative.success21@gmail.com>
2024-10-30 10:25:41 -0500

    final commit after testing from EC2

--- StreamLitApp.py
+++ StreamLitApp.py
@@ -11,7 +11,7 @@ from src.mcqgen.logger import logging
 
 
 
-with open('D:\Documents\Senthil\mcqg\Response.json', 'r') as file:
+with open('Response.json', 'r') as file:
     RESPONSE_JSON = json.load(file)
 
 
